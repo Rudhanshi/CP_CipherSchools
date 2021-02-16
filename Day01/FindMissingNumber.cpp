0d026414064cbcdcaa249e698e47c2c91948dcd9
Rudhanshi <42838545+Rudhanshi@users.noreply.github.com>
2021-02-16 13:25:50 +0530

    Create FindMissingNumber.cpp

---
+++ Day01/FindMissingNumber.cpp
@@ -0,0 +1,21 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int findMissing(vector<int> arr) {
+    int missing = 0;
+    for (int i = 0; i < arr.size(); i++) {
+        missing = missing ^ (i + 1) ^ arr[i]; 
+    }
+
+    missing = missing ^ (arr.size() + 1);
+    return missing;
+}
+
+int main(){
+   vector<int> arr ={1,2,3,4,5,6,8};
+   cout<<findMissing(arr);
+   return 0;
+     
+}
